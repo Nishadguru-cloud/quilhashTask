57fbb81c41de4df7b8c93984cd063a55e57e18e1
Nishad <snishadguru@gmail.com>
2020-05-31 00:29:12 +0530

    Quilhash task

---
+++ quilhash_task/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+export default () => {
+	return (
+		<div>
+		   Hi This is Nishad guru This is a simple application developed for Quilhash Task
+		   <Link to="/">Go Back to the application</Link>
+		</div>
+		);
+};
